151908f9ef3be926a9a3af0a925c18c9f949aa0e
Jaco <jacopo.decristofaro@external.eni.com>
2023-01-13 07:30:29 +0100

    Local run modification

--- util.py
+++ util.py
@@ -18,11 +18,11 @@ def move_to_device(optimizer: Type[torch.optim.Optimizer], device: str):
 def save_checkpoint(state: dict, is_best: bool, output_folder: str,
                     ckpt_filename: str = "last_checkpoint.pth"):
     # TODO it would be better to move weights to cpu before saving
-    if is_best:
-        torch.save(state["model_state_dict"], f"{output_folder}/best_model.pth")
     epoch_num = str(state["epoch_num"])
     checkpoint_path = f"{output_folder}/{ckpt_filename + epoch_num}"
-    torch.save(state, checkpoint_path)
+    torch.save(state["model_state_dict"], checkpoint_path)
+    if is_best:
+        torch.save(state["model_state_dict"], f"{output_folder}/best_model.pth")
     
 
 
