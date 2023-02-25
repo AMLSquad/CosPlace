4499118dba0a18be1f5c287580b4fa0fb3d85ee7
Lorediel <ld1@live.it>
2023-02-25 15:15:25 +0100

    augentations

--- augmentations.py
+++ augmentations.py
@@ -2,6 +2,8 @@
 import torch
 from typing import Tuple, Union
 import torchvision.transforms as T
+import torchvision.transforms.functional as TF
+import random
 
 
 class DeviceAgnosticColorJitter(T.ColorJitter):
@@ -35,6 +37,62 @@ class DeviceAgnosticRandomResizedCrop(T.RandomResizedCrop):
         return augmented_images
 
 
+class DeviceAgnosticColorJitter(T.ColorJitter):
+    def __init__(self, brightness: float = 0., contrast: float = 0., saturation: float = 0., hue: float = 0.):
+        """This is the same as T.ColorJitter but it only accepts batches of images and works on GPU"""
+        super().__init__(brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+    
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
+        assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
+        B, C, H, W = images.shape
+        # Applies a different color jitter to each image
+        color_jitter = super(DeviceAgnosticColorJitter, self).forward
+        augmented_images = [color_jitter(img).unsqueeze(0) for img in images]
+        augmented_images = torch.cat(augmented_images)
+        assert augmented_images.shape == torch.Size([B, C, H, W])
+        return augmented_images
+
+class DeviceAgosticAdjustBrightness(): 
+    def __init__(self, brightness_factor: float = 0.65):
+        self.brightness_factor = brightness_factor
+
+    def __call__(self, images: torch.Tensor) -> torch.Tensor:
+        assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
+        B, C, H, W = images.shape
+        # Applies a different color jitter to each image
+        offset = random.randint(-0.5, 0.5)
+        augmented_images = [TF.adjust_brightness(img, self.brightness_factor + offset ).unsqueeze(0) for img in images]
+        augmented_images = torch.cat(augmented_images)
+        assert augmented_images.shape == torch.Size([B, C, H, W])
+        return augmented_images
+    
+class DeviceAgnosticContrast(): 
+    def __init__(self, contrast_factor: float = 1.15):
+        self.contrast_factor = contrast_factor
+
+    def __call__(self, images: torch.Tensor) -> torch.Tensor:
+        assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
+        B, C, H, W = images.shape
+        # Applies a different color jitter to each image
+        offset = random.randint(-0.05, 0.05)
+        augmented_images = [TF.adjust_contrast(img, self.contrast_factor + offset ).unsqueeze(0) for img in images]
+        augmented_images = torch.cat(augmented_images)
+        assert augmented_images.shape == torch.Size([B, C, H, W])
+        return augmented_images
+    
+class DeviceAgnosticHue(): 
+    def __init__(self, hue_factor: float = 0.5):
+        self.hue_factor = hue_factor
+
+    def __call__(self, images: torch.Tensor) -> torch.Tensor:
+        assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
+        B, C, H, W = images.shape
+        # Applies a different color jitter to each image
+        augmented_images = [TF.adjust_hue(img, self.hue_factor ).unsqueeze(0) for img in images]
+        augmented_images = torch.cat(augmented_images)
+        assert augmented_images.shape == torch.Size([B, C, H, W])
+        return augmented_images
+    
 if __name__ == "__main__":
     """
     You can run this script to visualize the transformations, and verify that
@@ -45,13 +103,14 @@ if __name__ == "__main__":
     from skimage import data
     
     # Initialize DeviceAgnosticRandomResizedCrop
-    random_crop = DeviceAgnosticRandomResizedCrop(size=[256, 256], scale=[0.5, 1])
+    brightness = 0.5
+    augs = DeviceAgnosticHue()
     # Create a batch with 2 astronaut images
     pil_image = Image.fromarray(data.astronaut())
     tensor_image = T.functional.to_tensor(pil_image).unsqueeze(0)
     images_batch = torch.cat([tensor_image, tensor_image])
     # Apply augmentation (individually on each of the 2 images)
-    augmented_batch = random_crop(images_batch)
+    augmented_batch = augs(images_batch)
     # Convert to PIL images
     augmented_image_0 = T.functional.to_pil_image(augmented_batch[0])
     augmented_image_1 = T.functional.to_pil_image(augmented_batch[1])
