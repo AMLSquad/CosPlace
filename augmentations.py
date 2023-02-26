4249418d59dc099c27fa6dc00a940f9b37ab90ef
Lorediel <ld1@live.it>
2023-02-26 17:11:35 +0100

    adding desaturatin

--- augmentations.py
+++ augmentations.py
@@ -94,10 +94,11 @@ class DeviceAgnosticContrast():
         assert augmented_images.shape == torch.Size([B, C, H, W])
         return augmented_images
     
-class DeviceAgosticAdjustBrightnessAndContrast():
-    def __init__(self, brightness_factor: float = 0.65, contrast_factor: float = 1.15):
+class DeviceAgosticAdjustBrightnessContrastSaturation():
+    def __init__(self, brightness_factor: float = 0.65, contrast_factor: float = 1.15, saturation_factor: float = 1.15):
         self.brightness_factor = brightness_factor
         self.contrast_factor = contrast_factor
+        self.saturation_factor = saturation_factor
 
     def __call__(self, images: torch.Tensor) -> torch.Tensor:
         assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
@@ -105,12 +106,14 @@ class DeviceAgosticAdjustBrightnessAndContrast():
         # Applies a different color jitter to each image
         offsetBright = random.uniform(-0.5, 0.5)
         offsetContrast = random.uniform(-0.05, 0.05)
+        offsetSaturation = random.uniform(-0.05, 0.05)
         augmented_images = []
         for img in images:
             #transform with probability 50%
             if random.random() < 0.5:
                 augmented_img = TF.adjust_brightness(img, self.brightness_factor + offsetBright).unsqueeze(0)
                 augmented_img = TF.adjust_contrast(augmented_img, self.contrast_factor + offsetContrast ).unsqueeze(0)
+                augmented_img = TF.adjust_saturation(augmented_img, self.saturation_factor + offsetSaturation ).unsqueeze(0)
                 augmented_images.append(augmented_img)
             else:
                 augmented_images.append(img.unsqueeze(0))
@@ -119,8 +122,25 @@ class DeviceAgosticAdjustBrightnessAndContrast():
         return augmented_images
     
 class DeviceAgosticAdjustSaturation():
-    def __init__(self, contrast_factor: float = 1.15):
-        self.contrast_factor = contrast_factor
+    def __init__(self, saturation_factor: float = 0.85):
+        self.saturation_factor = saturation_factor
+
+    def __call__(self, images: torch.Tensor) -> torch.Tensor:
+        assert len(images.shape) == 4, f"images should be a batch of images, but it has shape {images.shape}"
+        B, C, H, W = images.shape
+        # Applies a different color jitter to each image
+        offset = random.uniform(-0.05, 0.05)
+        augmented_images = []
+        for img in images:
+            #transform with probability 50%
+            if random.random() < 0.5:
+                augmented_img = TF.adjust_saturation(img, self.saturation_factor + offset).unsqueeze(0)
+                augmented_images.append(augmented_img)
+            else:
+                augmented_images.append(img.unsqueeze(0))
+        augmented_images = torch.cat(augmented_images)
+        assert augmented_images.shape == torch.Size([B, C, H, W])
+        return augmented_images
     
     
 if __name__ == "__main__":
