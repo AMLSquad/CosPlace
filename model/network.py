905df66e6445784e543a85e5a0d70240cf6f3d3f
Lorediel <ld1@live.it>
2023-03-19 17:16:10 +0100

    fix error in network

--- model/network.py
+++ model/network.py
@@ -1,158 +1,118 @@
-"""
-Implements ADDA:
-Adversarial Discriminative Domain Adaptation, Tzeng et al. (2017)
-"""
-import argparse
 
 import torch
+import logging
+import torchvision
 from torch import nn
-from torch.utils.data import DataLoader
-from tqdm import tqdm, trange
-
-from model.network import GeoLocalizationNet
-from adda_utils import loop_iterable, set_requires_grad
-from datasets.train_dataset import TrainDataset
-from datasets.target_dataset import TargetDataset
-import os
+import copy
+from torch.autograd import Function
+from model.layers import Flatten, L2Norm, GeM
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+CHANNELS_NUM_IN_LAST_CONV = {
+    "resnet18": 512,
+    "resnet50": 2048,
+    "resnet101": 2048,
+    "resnet152": 2048,
+    "vgg16": 512,
+}
 
-class DatasetArgs:
-    def __init__(self, dataset_folder, pseudo_target_folder = None):
-        self.augmentation_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.dataset_folder = dataset_folder
-        self.pseudo_target_folder = pseudo_target_folder
-        self.brightness = 0.7
-        self.contrast = 0.7
-        self.saturation = 0.7
-        self.hue = 0.5
+CHANNELS_AFTER_AVGPOOLING = {
+    "resnet18": 512,
+    "resnet50": 2048,
+    "resnet101": 2048,
+    "resnet152": 2048,
+    "vgg16": 512,
+}
 
+class GradientReversalFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        return x.clone()
+    @staticmethod
+    def backward(ctx, grads):
+        dx = -grads.new_tensor(1) * grads
+        return dx, None
 
-def main(args):
+class GradientReversal(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        x = torch.nn.functional.adaptive_avg_pool2d(x, (1,1))
+        x = x.view(x.shape[0], -1)
+        return GradientReversalFunction.apply(x)
 
-    source_model = GeoLocalizationNet(args.backbone_name, args.fc_output_dim).to(device)
-    source_model.load_state_dict(torch.load(args.model_file))
-   
-    ds_args = DatasetArgs(args.source_dataset_path)
-    train_folder = os.path.join(args.source_dataset_path, "train")
-    source_dataset = TrainDataset(ds_args, train_folder)
-
-    
-    target_model = GeoLocalizationNet(args.backbone_name, args.fc_output_dim).to(device)
-    target_dataset = TargetDataset(args.target_dataset_path)
-
-    batch_size = args.batch_size
-    features_dim = args.features_dim
-
-    source_model = source_model.to(device)
-    source_model.eval()
-    set_requires_grad(source_model, requires_grad=False)
-    
-    clf = source_model
-    source_model = source_model.backbone
-
-    target_model = target_model.to(device)
-    target_model = target_model.backbone
-
+def get_discriminator(input_dim, num_classes=2):
     discriminator = nn.Sequential(
-        nn.Linear(features_dim, 50),
+        GradientReversal(),
+        nn.Linear(input_dim, 50),
         nn.ReLU(),
         nn.Linear(50, 20),
         nn.ReLU(),
-        nn.Linear(20, 1)
-    ).to(device)
+        nn.Linear(20, num_classes)
+    )
+    return discriminator
 
-    half_batch = batch_size // 2
 
-    source_loader = DataLoader(source_dataset, batch_size=half_batch,
-                               shuffle=True, num_workers=1, pin_memory=True)
+class GeoLocalizationNet(nn.Module):
+    def __init__(self, backbone, fc_output_dim, domain_adaptation = False):
+        super().__init__()
+        self.backbone, features_dim, _ = get_backbone(backbone)
+        self.aggregation = nn.Sequential(
+                L2Norm(),
+                # For each channel, get only one value
+                GeM(),
+                Flatten(),
+                nn.Linear(features_dim, fc_output_dim),
+                L2Norm()
+            )
+        # Domain adaptation
+        self.discriminator = get_discriminator(features_dim) if domain_adaptation == True else None
+        
+        
+
+
     
+    def forward(self, x, grl=False):
+        features = self.backbone(x)
+        if grl==True:
+            # perform adaptation round
+            # logits output dim is num_domains
+            return self.discriminator(features)
+
+        return self.aggregation(features)
+
+
+def get_backbone(backbone_name):
+    if backbone_name.startswith("resnet"):
+        if backbone_name == "resnet18":
+            backbone = torchvision.models.resnet18(pretrained=True)
+        elif backbone_name == "resnet50":
+            backbone = torchvision.models.resnet50(pretrained=True)
+        elif backbone_name == "resnet101":
+            backbone = torchvision.models.resnet101(pretrained=True)
+        elif backbone_name == "resnet152":
+            backbone = torchvision.models.resnet152(pretrained=True)
+        
+        for name, child in backbone.named_children():
+            if name == "layer3":  # Freeze layers before conv_3
+                break
+            for params in child.parameters():
+                params.requires_grad = False
+        logging.debug(f"Train only layer3 and layer4 of the {backbone_name}, freeze the previous ones")
+        avg_layer = list(backbone.children())[-2:-1]
+        layers = list(backbone.children())[:-2]  # Remove avg pooling and FC layer
     
-    target_loader = DataLoader(target_dataset, batch_size=half_batch,
-                               shuffle=True, num_workers=1, pin_memory=True)
-
-    discriminator_optim = torch.optim.Adam(discriminator.parameters())
-    target_optim = torch.optim.Adam(target_model.parameters())
-    criterion = nn.BCEWithLogitsLoss()
-
-    for epoch in range(1, args.epochs+1):
-        batch_iterator = zip(loop_iterable(source_loader), loop_iterable(target_loader))
-
-        total_loss = 0
-        total_accuracy = 0
-        for _ in trange(args.iterations, leave=False):
-            # Train discriminator
-            set_requires_grad(target_model, requires_grad=False)
-            set_requires_grad(discriminator, requires_grad=True)
-            for _ in range(args.k_disc):
-                (source_x), (target_x) = next(batch_iterator)
-                
-                source_x, target_x = source_x[0].to(device), target_x[0].to(device)
-
-                source_features = source_model(source_x)
-                source_features = torch.nn.functional.adaptive_avg_pool2d(source_features, (1,1)).view(source_x.shape[0], -1)
-                target_features = target_model(target_x)
-                target_features = torch.nn.functional.adaptive_avg_pool2d(target_features, (1,1)).view(target_x.shape[0], -1)
-
-                
-                discriminator_x = torch.cat([source_features, target_features])
-                
-                discriminator_y = torch.cat([torch.ones(source_x.shape[0], device=device),
-                                             torch.zeros(target_x.shape[0], device=device)])
-
-                preds = discriminator(discriminator_x).squeeze()
-                loss = criterion(preds, discriminator_y)
-
-                discriminator_optim.zero_grad()
-                loss.backward()
-                discriminator_optim.step()
-
-                total_loss += loss.item()
-                total_accuracy += ((preds > 0).long() == discriminator_y.long()).float().mean().item()
-
-            # Train classifier
-            set_requires_grad(target_model, requires_grad=True)
-            set_requires_grad(discriminator, requires_grad=False)
-            for _ in range(args.k_clf):
-                _, (target_x, _) = next(batch_iterator)
-                target_x = target_x.to(device)
-                target_features = target_model(target_x)
-                target_features = torch.nn.functional.adaptive_avg_pool2d(target_features, (1,1)).view(target_x.shape[0], -1)
-
-
-                # flipped labels
-                discriminator_y = torch.ones(target_x.shape[0], device=device)
-
-                preds = discriminator(target_features).squeeze()
-                loss = criterion(preds, discriminator_y)
-
-                target_optim.zero_grad()
-                loss.backward()
-                target_optim.step()
-
-        mean_loss = total_loss / (args.iterations*args.k_disc)
-        mean_accuracy = total_accuracy / (args.iterations*args.k_disc)
-        tqdm.write(f'EPOCH {epoch:03d}: discriminator_loss={mean_loss:.4f}, '
-                   f'discriminator_accuracy={mean_accuracy:.4f}')
-
-        # Create the full target model and save it
-        clf.backbone = target_model
-        torch.save(clf.state_dict(), 'trained_models/adda.pt')
-
-
-if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(description='Domain adaptation using ADDA')
-    arg_parser.add_argument('--backbone-name', default='resnet18')
-    arg_parser.add_argument('--fc-output-dim', type=int, default=512)
-    arg_parser.add_argument('--model_file')
-    arg_parser.add_argument('--source-dataset_path')
-    arg_parser.add_argument('--target-dataset_path')
-    arg_parser.add_argument('--features-dim', type=int, default=512)
-    arg_parser.add_argument('--batch-size', type=int, default=32)
-    arg_parser.add_argument('--iterations', type=int, default=500)
-    arg_parser.add_argument('--epochs', type=int, default=5)
-    arg_parser.add_argument('--k-disc', type=int, default=1)
-    arg_parser.add_argument('--k-clf', type=int, default=10)
-    args = arg_parser.parse_args()
-    main(args)
+    elif backbone_name == "vgg16":
+        backbone = torchvision.models.vgg16(pretrained=True)
+        avg_layer = list(backbone.features.children())[-2:-1]
+        layers = list(backbone.features.children())[:-2]  # Remove avg pooling and FC layer
+        for layer in layers[:-5]:
+            for p in layer.parameters():
+                p.requires_grad = False
+        logging.debug("Train last layers of the VGG-16, freeze the previous ones")
+    
+    backbone = torch.nn.Sequential(*layers)
+    
+    features_dim = CHANNELS_NUM_IN_LAST_CONV[backbone_name]
+    
+    return backbone, features_dim, avg_layer
