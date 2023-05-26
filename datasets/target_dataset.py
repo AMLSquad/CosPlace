68f9aaa63ade9ab85e358b0dfc14e2865023c7fb
Lorediel <ld1@live.it>
2023-05-26 09:24:03 +0200

    fix branch

--- datasets/target_dataset.py
+++ datasets/target_dataset.py
@@ -40,7 +40,7 @@ class TargetDataset(data.Dataset):
 class DomainAdaptationDataLoader(data.DataLoader):
     def __init__(self, source_dataset, target_dataset, *args, **kwargs):
 
-        self.source_dim = int(kwargs["batch_size"] * 2 / 3)
+        self.source_dim = int(kwargs["batch_size"] * 1 / 2)
         self.target_dim = kwargs["batch_size"] - self.source_dim
         del kwargs["batch_size"]
         self.source_domain_loader = data.DataLoader(source_dataset, batch_size=self.source_dim, **kwargs)
@@ -54,13 +54,9 @@ class DomainAdaptationDataLoader(data.DataLoader):
     def  __next__(self):
         try:
             source_images,_,_,source_domain_labels = next(self.source_domain_iterator)
-            source_images = source_images[source_domain_labels == 0]
-            source_domain_labels = source_domain_labels[source_domain_labels == 0]
         except StopIteration:
             self.source_domain_iterator = self.source_domain_loader.__iter__()
             source_images,_,_,source_domain_labels = next(self.source_domain_iterator)
-            source_images = source_images[source_domain_labels == 0]
-            source_domain_labels = source_domain_labels[source_domain_labels == 0]
         try:
             target_images,target_domain_labels = next(self.target_domain_iterator)
         except:
