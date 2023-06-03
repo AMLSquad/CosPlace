9a7c5cc90329ab595ac087ad5402934501189a66
Lorediel <ld1@live.it>
2023-06-03 10:18:40 +0200

    fix

--- train.py
+++ train.py
@@ -266,6 +266,7 @@ if __name__ == "__main__":
                     pseudo_descriptors = model(pseudo_images)
                     final_descriptors = torch.cat((descriptors, pseudo_descriptors), 0)
                     final_targets = torch.cat((targets, pseudo_targets), 0)
+                    print(final_descriptors.shape)
                     output = classifiers[current_group_num](final_descriptors, final_targets)
                 else:
                     output = classifiers[current_group_num](descriptors, targets)
